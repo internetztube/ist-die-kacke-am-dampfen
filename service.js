c3b11c08d220032023730bc07ecd7d4db1119f4a
Frederic Koeberl <frederic.koeberl@dynatrace.com>
2021-10-10 19:35:23 +0200

    final touches

--- service.js
+++ service.js
@@ -18,7 +18,7 @@ const service = async () => {
     const browser = await puppeteer.launch({
         headless: true,
         args: ['--no-sandbox'],
-        defaultViewport: { width: 1100,  height: 800, deviceScaleFactor: 2 }
+        defaultViewport: { width: 1100,  height: 700, deviceScaleFactor: 2 }
     });
     const page = await browser.newPage()
     await page.setJavaScriptEnabled(false)
